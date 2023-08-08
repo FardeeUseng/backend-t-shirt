9b0ce1d0064d32f297b96cbc475615d3e915a782
Dee <fardee.tat@gmail.com>
2023-08-08 16:31:19 +0700

    start project

---
+++ app/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load the .env file
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	// get database connection
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSslMode := os.Getenv("DB_SSL_MODE")
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", dbHost, dbPort, dbUser, dbPassword, dbName, dbSslMode)
+
+	db, err := sqlx.Open("pgx", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
